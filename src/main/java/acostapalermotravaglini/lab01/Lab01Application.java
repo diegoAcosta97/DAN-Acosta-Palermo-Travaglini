362338d20ae960852f0fcb8fd49aae33d4ac0a14
Diego Acosta <dacosta@frsf.utn.edu.ar>
2021-06-01 19:29:44 -0300

    Agregado de clases de dominio.

--- src/main/java/acostapalermotravaglini/lab01/Lab01Application.java
+++ src/main/java/acostapalermotravaglini/lab01/Lab01Application.java
@@ -2,12 +2,17 @@ package acostapalermotravaglini.lab01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class Lab01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab01Application.class, args);
+
     }
+
+
 
 }
